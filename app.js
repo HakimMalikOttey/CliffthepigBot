60d5601ed98ab0d7a420cc9ffe086008dc3a0f55
HakimMalikOttey <35748681+HakimMalikOttey@users.noreply.github.com>
2025-02-19 01:05:39 -0500

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+var http = require('http')
+http.createServer(function(request,response){
+    const client = new tmi.Client({
+        channels: [ 'cliffthepig' ]
+    });
+    
+    client.connect();
+    let count = 0;
+    let listeningForCount = false;
+    const users = {}
+    client.on('message', (channel, tags, message, self) => {
+        const {username } = tags;
+        if(self) return true;
+        if(username === 'cliffthepig' && message === '!start-count'){
+            listeningForCount = true;
+        }
+        else if(username === 'cliffthepig' && message === '!end-count'){
+            listeningForCount = false;
+        }
+        else if(listeningForCount && message == '1'){
+            users[tags.username] = true;
+        }
+    
+    });
+}).listen(process.env.PORT)
